7ac01064188f88f6704a4df6de5efc4aa7d38a35
Michael Nongphud <michael_n@pursuitsoftware.biz>
2025-01-03 13:22:17 +0530

    Added Project

---
+++ mnNodeAppTest/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use("/", (req, res) => {
+    res.send("Server started")
+});
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000")
+});
